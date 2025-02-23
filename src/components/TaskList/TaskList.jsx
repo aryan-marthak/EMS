279578be74f08d2780ffc5802acfdbb907656d34
aryan-marthak <aryan.marthak1@gmail.com>
2025-02-23 22:52:22 +0530

    header section

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TaskList = () => {
+    return (
+        <div className='h-[55%] w-full bg-red-500 mt-10 py-5'>
+            <div className='h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+            </div>
+        </div>
+    )
+}
+
+export default TaskList
