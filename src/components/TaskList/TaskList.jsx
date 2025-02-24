4835bead0bd6538f6e9a4634f5df4abe776f1d1e
aryan-marthak <aryan.marthak1@gmail.com>
2025-02-24 22:16:33 +0530

    dashboard progress

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,9 +2,46 @@ import React from 'react'
 
 const TaskList = () => {
     return (
-        <div className='h-[55%] w-full bg-red-500 mt-10 py-5'>
-            <div className='h-full w-[300px] bg-yellow-400 rounded-xl'>
-
+        <div id='taskList' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full mt-10 py-5'>
+            <div className=' flex-shrink-0 h-full p-7 w-[334px] bg-red-400 rounded-xl'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                    <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+                </div>
+                <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+                <p className='text-base mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, magni laborum. Incidunt aliquam nobis illum.</p>
+            </div>
+            <div className=' flex-shrink-0 h-full p-7 w-[334px] bg-blue-400 rounded-xl'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                    <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+                </div>
+                <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+                <p className='text-base mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, magni laborum. Incidunt aliquam nobis illum.</p>
+            </div>
+            <div className=' flex-shrink-0 h-full p-7 w-[334px] bg-green-400 rounded-xl'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                    <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+                </div>
+                <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+                <p className='text-base mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, magni laborum. Incidunt aliquam nobis illum.</p>
+            </div>
+            <div className=' flex-shrink-0 h-full p-7 w-[334px] bg-yellow-400 rounded-xl'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                    <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+                </div>
+                <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+                <p className='text-base mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, magni laborum. Incidunt aliquam nobis illum.</p>
+            </div>
+            <div className=' flex-shrink-0 h-full p-7 w-[334px] bg-red-400 rounded-xl'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+                    <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+                </div>
+                <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+                <p className='text-base mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, magni laborum. Incidunt aliquam nobis illum.</p>
             </div>
         </div>
     )
--- src/components/other/TaskListNumbers.jsx
+++ src/components/other/TaskListNumbers.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const TaskListNumbers = () => {
     return (
-        <div className='flex mt-10 justify-between gap-5 '>
-            <div className='py-6 px-9 rounded-xl bg-red-400 w-[45%]'>
-                <h2 className=' text-3xl font-semibold'>0</h2>
-                <h3 className='text-xl font-medium'>New Task</h3>
+        <div className='flex mt-10 justify-between gap-10 '>
+            <div className='py-6 px-9 rounded-xl bg-red-400 w-[30%]'>
+                <h2 className=' text-4xl font-bold'>0</h2>
+                <h3 className='text-2xl mt-1 font-semibold'>New Task</h3>
             </div>
-            <div className='py-6 px-9 rounded-xl bg-blue-400 w-[45%]'>
-                <h2 className=' text-3xl font-semibold'>0</h2>
-                <h3 className='text-xl font-medium'>New Task</h3>
+            <div className='py-6 px-9 rounded-xl bg-blue-400 w-[30%]'>
+                <h2 className=' text-4xl font-bold'>0</h2>
+                <h3 className='text-2xl mt-1 font-semibold'>Completed Task</h3>
             </div>
-            <div className='py-6 px-9 rounded-xl bg-green-400 w-[45%]'>
-                <h2 className=' text-3xl font-semibold'>0</h2>
-                <h3 className='text-xl font-medium'>New Task</h3>
+            <div className='py-6 px-9 rounded-xl bg-green-400 w-[30%]'>
+                <h2 className=' text-4xl font-bold'>0</h2>
+                <h3 className='text-2xl mt-1 font-semibold'>Accepted Task</h3>
             </div>
-            <div className='py-6 px-9 rounded-xl bg-yellow-400 w-[45%]'>
-                <h2 className=' text-3xl font-semibold'>0</h2>
-                <h3 className='text-xl font-medium'>New Task</h3>
+            <div className='py-6 px-9 rounded-xl bg-yellow-400 w-[30%]'>
+                <h2 className=' text-4xl font-bold'>0</h2>
+                <h3 className='text-2xl mt-1 font-semibold'>Failed Task</h3>
             </div>
         </div>
     )
