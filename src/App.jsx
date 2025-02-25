da9be623f4df9bc38ed2bc8c03caa05c80ae1e53
aryan-marthak <aryan.marthak1@gmail.com>
2025-02-25 23:18:36 +0530

    AUTH

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { setLocalStorage } from './utils/localStorage'
 
 const App = () => {
+  const [user, setUser] = useState(null)
+
+  const handleLogin = (email, password) => {
+    if (email === 'admin@menubar.com' && password === '123') {
+      setUser('admin')
+    }
+    else if (email === 'user@me.com' && password === '123') {
+      setUser('employee')
+    }
+    else {
+      alert('invalid credentials')
+    }
+  }
+
   return (
     <>
-
-      {/* <Login />  */}
-      {/* <EmployeeDashboard /> */}
-      <AdminDashboard />
-
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user === 'admin' ? <AdminDashboard /> : user === 'employee' ? <EmployeeDashboard/> : null}
     </>
   )
 }
